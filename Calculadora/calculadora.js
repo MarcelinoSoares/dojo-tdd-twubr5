7a5f9c550d35a04bda8b7735c0f350cb6980d87e
Marcelino Soares de Oliveira <43478173+MarcelinoSoares@users.noreply.github.com>
2022-04-29 13:44:02 -0300

    adicionando as operaçao e testes da calculadora

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,6 +1,22 @@
 class Calculadora {
-  static soma() {
-    //to-do
+  static soma(numero1, numero2) {
+    return numero1 + numero2;
+  }
+
+   static subtracao(numero1, numero2) {
+    return numero1 - numero2;
+   }
+  
+   static multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+   }
+  
+   static divisao(numero1, numero2) {
+    return numero1 / numero2;
+   }
+  
+    static expressao(numero1, numero2) {
+    return numero1 + numero2 / numero1 - numero2;
   }
 }
 
--- Calculadora/calculadora.test.js
+++ Calculadora/calculadora.test.js
@@ -1,7 +1,41 @@
 const Calculadora = require("./calculadora");
 
 describe("Calculadora", () => {
+  const numero1 = 10;
+  const numero2 = 5;
+
   test("Deve realizar a soma dos dois valores recebidos", () => {
-    // to-do
+    const resultadoEsperado = 15;
+    const resultado = Calculadora.soma(numero1,numero2);
+
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+   test("Deve realizar a subtracao dos dois valores recebidos", () => {
+    const resultadoEsperado = 5;
+    const resultado = Calculadora.subtracao(numero1,numero2);
+
+    expect(resultado).toBe(resultadoEsperado);
+   });
+  
+   test("Deve realizar a multiplicacao dos dois valores recebidos", () => {
+    const resultadoEsperado = 50;
+    const resultado = Calculadora.multiplicacao(numero1,numero2);
+
+    expect(resultado).toBe(resultadoEsperado);
+   });
+  
+   test("Deve realizar a divisao dos dois valores recebidos", () => {
+    const resultadoEsperado = 2;
+    const resultado = Calculadora.divisao(numero1,numero2);
+
+    expect(resultado).toBe(resultadoEsperado);
+   });
+  
+     test("Deve realizar a expressao dos dois valores recebidos", () => {
+    const resultadoEsperado = 5.5;
+    const resultado = Calculadora.expressao(numero1,numero2);
+
+    expect(resultado).toBe(resultadoEsperado);
   });
 });
